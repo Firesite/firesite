f8ac5ba583cf3e786061f02eae578e20c9411ce2
Adriaan van der Bergh <avanderbergh@gmail.com>
2019-12-22 16:17:04 +0100

    fixed .gitignore

--- functions/run-command.js
+++
@@ -1,7 +0,0 @@
-const { exec } = require("promisify-child-process");
-
-const runCommand = (command, directory) => {
-	return exec(command, { cwd: directory });
-};
-
-exports.runCommand = runCommand;
